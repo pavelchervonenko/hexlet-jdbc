8b4851ea0b4498a3021f00bb55066bcea9c19c9e
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-08-04 01:09:09 +0300

    meow

---
+++ hexlet-jdbc/build.gradle.kts
@@ -0,0 +1,26 @@
+plugins {
+    id("java")
+    id("application")
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("hexlet.code.Application")
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+    implementation("com.h2database:h2:2.2.220")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
+
