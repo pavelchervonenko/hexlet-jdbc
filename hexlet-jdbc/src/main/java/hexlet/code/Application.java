d69c657f5bca0463c7702013cc50b022c1e73c5d
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-08-04 03:18:06 +0300

    add preparedStatement

--- hexlet-jdbc/src/main/java/hexlet/code/Application.java
+++ hexlet-jdbc/src/main/java/hexlet/code/Application.java
@@ -2,30 +2,47 @@ package hexlet.code;
 
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Application {
     public static void main(String[] args) throws SQLException {
         try (var conn = DriverManager.getConnection("jdbc:h2:mem:hexlet_test")) {
 
-            var sql = "CREATE TABLE users ("
+            var sqlCreate = "CREATE TABLE users ("
                     + "id BIGINT PRIMARY KEY AUTO_INCREMENT,"
                     + "username VARCHAR(255),"
                     + "phone VARCHAR(255))";
 
             try (var statement = conn.createStatement()) {
-                statement.execute(sql);
+                statement.execute(sqlCreate);
             }
 
 
-            var sql2 = "INSERT INTO users (username, phone) VALUES ('tommy', '123456789')";
-            try (var statement2 = conn.createStatement()) {
-                statement2.executeUpdate(sql2);
+            var sqlInsertNewUsers = "INSERT INTO users (username, phone) VALUES (?, ?)";
+            try (var preparedStatement = conn.prepareStatement(sqlInsertNewUsers)) {
+                preparedStatement.setString(1, "Tommy");
+                preparedStatement.setString(2, "79052466597");
+                preparedStatement.executeUpdate();
+
+                preparedStatement.setString(1, "Ksenia");
+                preparedStatement.setString(2, "79022466598");
+                preparedStatement.executeUpdate();
+
+                preparedStatement.setString(1, "Kirill");
+                preparedStatement.setString(2, "89067899876");
+                preparedStatement.executeUpdate();
+            }
+
+            var sqlDelete = "DELETE FROM users WHERE username = ?";
+            try (var preparedStatement = conn.prepareStatement(sqlDelete)) {
+                preparedStatement.setString(1, "Kirill");
+                preparedStatement.executeUpdate();
             }
 
 
-            var sql3 = "SELECT * FROM users";
+            var sqlSelect = "SELECT * FROM users";
             try (var statement3 = conn.createStatement()) {
-                var resultSet = statement3.executeQuery(sql3);
+                var resultSet = statement3.executeQuery(sqlSelect);
 
                 while (resultSet.next()) {
                     System.out.println(resultSet.getLong("id"));
